7e9416e95ca71477640faf787161f59bad1a9872
WarlockGames12 <fpweeyou@gmail.com>
2022-05-24 09:24:47 +0200

    level 1

--- Assets/Scripts/PlayerHitsWithRay.cs
+++ Assets/Scripts/PlayerHitsWithRay.cs
@@ -8,7 +8,7 @@ public class PlayerHitsWithRay : MonoBehaviour
 {
 
     public GameObject[] Bendables;
-    public Transform[] Bendables1;
+    private Transform[] Bendables1;
 
     private bool isRotated0 = false;
     private bool isRotated1 = false;
@@ -19,12 +19,12 @@ public class PlayerHitsWithRay : MonoBehaviour
 
     private void Start()
     {
-        Bendables1[0].transform.rotation = Bendables[0].transform.rotation;
-        Bendables1[1].transform.rotation = Bendables[1].transform.rotation;
-        Bendables1[2].transform.rotation = Bendables[2].transform.rotation;
-        Bendables1[3].transform.rotation = Bendables[3].transform.rotation;
-        Bendables1[4].transform.rotation = Bendables[4].transform.rotation;
-        Bendables1[5].transform.rotation = Bendables[5].transform.rotation;
+        Bendables1 = new Transform[Bendables.Length]; 
+
+        for (int i = 0; i < Bendables.Length; i++)
+        {
+            Bendables1[i] = Bendables[i].transform;
+        }
     }
 
     // Update is called once per frame
@@ -32,90 +32,77 @@ public class PlayerHitsWithRay : MonoBehaviour
     {
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        if (Bendables1[0].transform.rotation.z == 0)
-        {
-            isRotated0 = true;
-        }
-        if (Bendables1[1].transform.rotation.z == -90)
-        {
-            isRotated1 = true;
-        }
-        if (Bendables1[2].transform.rotation.z == 180)
-        {
-            isRotated2 = true;
-        }
-        if (Bendables1[3].transform.rotation.z == 90)
-        {
-            isRotated3 = true;
-        }
-        if (Bendables1[4].transform.rotation.z == 90)
-        {
-            isRotated4 = true;
-        }
-        if (Bendables1[5].transform.rotation.z == -90)
-        {
-            isRotated5 = true;
-        }
-
-        if (Physics.Raycast(ray, out hit))
-        {
+ 
             if (Input.GetMouseButtonDown(0))
             {
+              if (Physics.Raycast(ray, out hit))
+              {
+                Debug.ClearDeveloperConsole();
                 if (hit.collider.CompareTag("Bendable") && !isRotated0)
                 {
                     Bendables[0].transform.Rotate(0, 0, 90);
+                    Debug.Log(Bendables[0].transform.localEulerAngles);
+                    if (Bendables[0].transform.localEulerAngles.y == 0)
+                    {
+                        Debug.Log("Pole0");
+                        isRotated0 = true;
+                    }
                 }
-                else if (hit.collider.CompareTag("Bendable") && isRotated0)
-                {
-                    Debug.Log("Don't Rotate");
-                }
-                if (hit.collider.CompareTag("Bendable1"))
+                if (hit.collider.CompareTag("Bendable1") && !isRotated1)
                 {
                     Bendables[1].transform.Rotate(0, 0, 90);
+                    Debug.Log(Bendables[1].transform.localEulerAngles);
+                    if (Bendables[1].transform.localEulerAngles.y == 270 || Bendables[1].transform.localEulerAngles.y == -90)
+                    {
+                        Debug.Log("Pole1");
+                        isRotated1 = true;
+                    }
                 }
-                else if (hit.collider.CompareTag("Bendable1") && isRotated1)
-                {
-                    Debug.Log("Don't Rotate1");
-                }
-                if (hit.collider.CompareTag("Bendable2"))
+                if (hit.collider.CompareTag("Bendable2") && !isRotated2)
                 {
                     Bendables[2].transform.Rotate(0, 0, 90);
+                    Debug.Log(Bendables[2].transform.localEulerAngles);
+                    if (Bendables[2].transform.localEulerAngles.y == 180 || Bendables[2].transform.localEulerAngles.y == -180)
+                    {
+                        Debug.Log("Pole2");
+                        isRotated2 = true;
+                    }
                 }
-                else if (hit.collider.CompareTag("Bendable2") && isRotated2)
-                {
-                    Debug.Log("Don't Rotate2");
-                }
-                if (hit.collider.CompareTag("Bendable3"))
+                if (hit.collider.CompareTag("Bendable3") && !isRotated3)
                 {
                     Bendables[3].transform.Rotate(0, 0, 90);
+                    Debug.Log(Bendables[3].transform.localEulerAngles);
+                    if (Bendables[3].transform.localEulerAngles.y == 90 || Bendables[3].transform.localEulerAngles.y == -270)
+                    {
+                        Debug.Log("Pole3");
+                        isRotated3 = true;
+                    }
                 }
-                else if (hit.collider.CompareTag("Bendable3") && isRotated3)
-                {
-                    Debug.Log("Don't Rotate3");
-                }
-                if (hit.collider.CompareTag("Bendable4"))
+                if (hit.collider.CompareTag("Bendable4") && !isRotated4)
                 {
                     Bendables[4].transform.Rotate(0, 0, 90);
+                    Debug.Log(Bendables[4].transform.localEulerAngles);
+                    if (Bendables[4].transform.localEulerAngles.y == 90 || Bendables[4].transform.localEulerAngles.y == -270)
+                    {
+                        Debug.Log("Pole4");
+                        isRotated4 = true;
+                    }
                 }
-                else if (hit.collider.CompareTag("Bendable4") && isRotated4)
-                {
-                    Debug.Log("Don't Rotate4");
-                }
-                if (hit.collider.CompareTag("Bendable5"))
+                if (hit.collider.CompareTag("Bendable5") && !isRotated5)
                 {
                     Bendables[5].transform.Rotate(0, 0, 90);
-                }
-                else if (hit.collider.CompareTag("Bendable5") && isRotated5)
-                {
-                    Debug.Log("Don't Rotate5");
+                    Debug.Log(Bendables[5].transform.localEulerAngles);
+                    if (Bendables[5].transform.localEulerAngles.y == 270 || Bendables[5].transform.localEulerAngles.y == -90)
+                    {
+                        Debug.Log("Pole5");
+                        isRotated5 = true;
+                    }
                 }
             }
-            if (isRotated0 && isRotated1 && isRotated2 && isRotated3 && isRotated4 && isRotated5)
-            {
-                SceneManager.LoadScene("SampleScene");
-            }
-            
+        }
+        if (isRotated0 && isRotated1 && isRotated2 && isRotated3 && isRotated4 && isRotated5)
+        { 
+            SceneManager.LoadScene("SampleScene");
         }
     }
 
