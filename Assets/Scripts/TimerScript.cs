9c035a55d06ab3d9001c852de776ec6d410c60fb
WarlockGames12 <fpweeyou@gmail.com>
2022-06-09 10:32:42 +0200

    Game Over

--- Assets/Scripts/TimerScript.cs
+++ Assets/Scripts/TimerScript.cs
@@ -8,28 +8,44 @@ public class TimerScript : MonoBehaviour
     [Header("Timer Settings:")]
     public Text Timer;
     public float countdown = 180f;
+    public bool isRunning = false;
 
     [Header("Dont Destroy Music")]
     public GameObject DontDestroy;
+    public GameObject GameOver;
+
+    [Header("Play Rotation after Game is over")]
+    public Animation Model;
 
     void Start()
     {
         DontDestroy = GameObject.Find("Hover");
+        GameOver.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (countdown <= 0f)
+        if (!isRunning)
         {
-            ResetGame();
+            if (countdown <= 0f)
+            {
+                ResetGame();
+            }
+            Timer.text = Mathf.Round(countdown).ToString();
+            countdown -= Time.deltaTime;
         }
-        Timer.text = Mathf.Round(countdown).ToString();
-        countdown -= Time.deltaTime;
     }
     public void ResetGame()
     {
-        
+        GameOver.SetActive(true);
+        Model.Play();
+        isRunning = true;
+    }
+
+    public void BackToMenu()
+    {
+        Destroy(DontDestroy);
         SceneManager.LoadScene("Start");
         print("Reset Scene");
     }
